6400fd8c1958f7dfe65f10070a8c0534c909bdac
Michael Tombor <michaeltombor@Mac-Studio.local>
2023-09-19 09:57:59 -0700

    added private key

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "025f042630fc5d7b05b7c87b151b09fa07f09fcf1a025d1d30512003864d60cc47": 100,
+  "0214eeb3d274b17081c229d656ca1967cb4bb9a291f85d0af98ef6a1b97cd07217": 50,
+  "02b066969240c947a85463b70b4316a758b396af44086d0fa3b19042f9bc07188b": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
